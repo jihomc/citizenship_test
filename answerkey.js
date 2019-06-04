fcfe8e1f97b91b7e0f5b0b685ad4b0c7eb994f55
Michael Clegg <michaelclegg8@gmail.com>
2019-06-03 18:52:52 -0700

    sync repository

---
+++ answerkey.js
@@ -0,0 +1,291 @@
+var testUpdatesGen = {
+        "28. What is the name of the President of the United States now?*": ["Donald J. Trump", "Donald Trump", "Trump"],
+        "29. What is the name of the Vice President of the United States now?": ["Michael R. Pence", "Mike Pence", "Pence"],
+        "39. How many justices are on the Supreme Court?": "nine (9)",
+        "40. Who is the Chief Justice of the United States now?": ["John Roberts", "John G. Roberts, Jr."],
+        "46. What is the political party of the President now?": "Republican (Party)",
+        "47. What is the name of the Speaker of the House of Representatives now?": ["Nancy Pelosi", "Pelosi"],
+    }
+
+var testUpdatesGeo = {
+"Alabama": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Doug Jones", "Richard C. Shelby"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Kay Ivey"
+},
+"Alaska": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Lisa Murkowski", "Dan Sullivan"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Mike Dunleavy"
+},
+"American Samoa": {
+    "20. Who is one of your state’s U.S. Senators now?*": "American Samoa has no U.S. Senators.",
+    "23. Name your U.S. Representative.": "American Samoa has no (voting) Representatives in Congress.",
+    "43. Who is the Governor of your state now?": "Lolo Matalasi Moliga"
+},
+"Arizona": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Martha McSally", "Kyrsten Sinema"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Doug Ducey"
+},
+"Arkansas": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["John Boozman", "Tom Cotton"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Asa Hutchinson"
+},
+"California": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Dianne Feinstein", "Kamala D. Harris"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Gavin Newsom"
+},
+"Colorado": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Michael F. Bennet", "Cory Gardner"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Jared Polis"
+},
+"Connecticut": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Richard Blumenthal", "Christopher Murphy"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Ned Lamont"
+},
+"Delaware": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Thomas R. Carper", "Christopher A. Coons"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "John Carney"
+},
+"District of Columbia": {
+    "20. Who is one of your state’s U.S. Senators now?*": "D.C. has no U.S. Senators.",
+    "23. Name your U.S. Representative.": "Washington D.C. has no (voting) Representatives in Congress.",
+    "43. Who is the Governor of your state now?": "D.C. does not have a Governor."
+},
+"Florida": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Marco Rubio", "Rick Scott"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Ron DeSantis"
+},
+"Georgia": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Johnny Isakson", "David Perdue"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Brian Kemp"
+},
+"Guam": {
+    "20. Who is one of your state’s U.S. Senators now?*": "Guam has no U.S. Senators",
+    "23. Name your U.S. Representative.": "Guam has no (voting) Representatives in Congress.",
+    "43. Who is the Governor of your state now?": "Lou Leon Guerrero"
+},
+"Hawaii": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Mazie K. Hirono", "Brian Schatz"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "David Ige"
+},
+"Idaho": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Mike Crapo", "James E. Risch"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Brad Little"
+},
+"Illinois": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Tammy Duckworth", "Richard J. Durbin"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": " JB Pritzker"
+},
+"Indiana": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Mike Braun", "Todd Youn"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Eric Holcomb"
+},
+"Iowa": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Joni Ernst", "Chuck Grassley"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Kim Reynolds"
+},
+"Kansas": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Jerry Moran", "Pat Roberts"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Laura Kelly"
+},
+"Kentucky": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Mitch McConnell", "Rand Paul"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Matt Bevin"
+},
+"Louisiana": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Bill Cassidy", "John Kennedy"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "John Bel Edwards"
+},
+"Maine": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Angus S. King, Jr.", "Susan M. Collins"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Janet Mills"
+},
+"Maryland": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Benjamin L. Cardin", "Chris Van Hollen"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Larry Hogan"
+},
+"Massachusetts": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Edward J. Markey", "Elizabeth Warren"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Charlie Baker"
+},
+"Michigan": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Debbie Stabenow", "Gary C. Peters"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Gretchen Whitmer"
+},
+"Minnesota": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Amy Klobuchar", "Tina Smith"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Tim Walz"
+},
+"Mississippi": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Roger F. Wicker", "Cindy Hyde-Smith"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Phil Bryant"
+},
+"Missouri": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Josh Hawley", "Roy Blunt"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Mike Parson"
+},
+"Montana": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Jon Tester", "Steve Daines"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Steve Bullock"
+},
+"Nebraska": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Deb Fischer", "Ben Sasse"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Pete Ricketts"
+},
+"Nevada": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Jacky Rosen", "Catherine Cortez Masto"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Steve Sisolak"
+},
+"New Hampshire": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Jeanne Shaheen", "Margaret Wood Hassan"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Chris Sununu"
+},
+"New Jersey": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Robert Menendez", "Cory A. Booker"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Phil Murphy"
+},
+"New Mexico": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Martin Heinrich", "Tom Udall"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Michelle Lujan Grisham"
+},
+"New York": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Kirsten E. Gillibrand", "Charles E. Schumer"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Andrew Cuomo"
+},
+"North Carolina": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Thom Tillis", "Richard Burr"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Roy Cooper"
+},
+"North Dakota": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Kevin Cramer", "John Hoeven"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Doug Burgum"
+},
+"Northern Mariana Islands": {
+    "20. Who is one of your state’s U.S. Senators now?*": "Northern Mariana Islands has no U.S. Senators",
+    "23. Name your U.S. Representative.": "Northern Mariana Islands has no (voting) Representatives in Congress.",
+    "43. Who is the Governor of your state now?": "Ralph Deleon Guerrero Torres"
+},
+"Ohio": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Sherrod Brown", "Rob Portman"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Mike DeWine"
+},
+"Oklahoma": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["James M. Inhofe", "James Lankford"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Kevin Stitt"
+},
+"Oregon": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Jeff Merkley", "Ron Wyden"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Kate Brown"
+},
+"Pennsylvania": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Robert P. Casey, Jr.", "Patrick J. Toomey"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Tom Wolf"
+},
+"Puerto Rico": {
+    "20. Who is one of your state’s U.S. Senators now?*": "Puerto Rico has no U.S. Senators",
+    "23. Name your U.S. Representative.": "Puerto Rico has no (voting) Representatives in Congress.",
+    "43. Who is the Governor of your state now?": "Ricardo Rosselló"
+},
+"Rhode Island": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Sheldon Whitehouse", "Jack Reed"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Gina Raimondo"
+},
+"South Carolina": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Lindsey Graham", "Tim Scott"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Henry McMaster"
+},
+"South Dakota": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Mike Rounds", "John Thune"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Kristi Noem"
+},
+"Tennessee": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Marsha Blackburn", "Lamar Alexander"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Bill Lee"
+},
+"Texas": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Ted Cruz", "John Cornyn"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Greg Abbott"
+},
+"Utah": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Mitt Romney", "Mike Lee"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Gary Herbert"
+},
+"Vermont": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Bernard Sanders","Patrick J. Leahy"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Phil Scott"
+},
+"Virginia": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Tim Kaine", "Mark R. Warner"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Ralph Northam"
+},
+"Virgin Islands": {
+    "20. Who is one of your state’s U.S. Senators now?*": "Virgin Islands has no U.S. Senators",
+    "23. Name your U.S. Representative.": "Virgin Islands has no (voting) Representatives in Congress.",
+    "43. Who is the Governor of your state now?": "Albert Bryan"
+},
+"Washington": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Maria Cantwell", "Patty Murray"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Jay Inslee"
+},
+"West Virginia": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Joe Manchin III", "Shelley Moore Capito"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Jim Justice"
+},
+"Wisconsin": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["Tammy Baldwin", "Ron Johnson"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Tony Evers"
+},
+"Wyoming": {
+    "20. Who is one of your state’s U.S. Senators now?*": ["John Barrasso", "Michael B. Enzi"],
+    "23. Name your U.S. Representative.": "",
+    "43. Who is the Governor of your state now?": "Mark Gordon"
+}
+}
