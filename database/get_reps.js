3544e3de0594b1fb26cd61784613a8fe9124be75
Michael Clegg <michaelclegg8@gmail.com>
2019-06-19 22:30:21 -0700

    scraped reps & zips to html and created list of zips

--- database/get_reps.js
+++ database/get_reps.js
@@ -1,7 +1,3 @@
-import { REPLServer } from "repl";
-import { Z_PARTIAL_FLUSH } from "zlib";
-import { SIGPIPE } from "constants";
-
 // [Residents of territories with nonvoting Delegates or Resident Commissioners may provide 
 // the name of that Delegate or Commissioner. Also acceptable is any statement that the 
 // territory has no (voting) Representatives in Congress.]
@@ -18,90 +14,184 @@ import { SIGPIPE } from "constants";
 // location
 // question_id
 
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const fips = require('./fips_states.js');
 
-// Step 1: Scrape member data from house.gov http://clerk.house.gov/xml/lists/MemberData.xml
+// array format
+let replist = [];
+let ziplist = [];
+// object format
+let repobj = {};
+let zipobj = {};
+
+// scrape member data from house.gov http://clerk.house.gov/xml/lists/MemberData.xml
+// scrape natl_zccd_delim.txt from census.gov
+// write html to files, load into cheerio and return results for parsing
+
+
+const build_reps = async() => {
+
+    const get_reps = {
+                uri: 'http://clerk.house.gov/xml/lists/MemberData.xml',
+                transform: function(body) {
+                    return cheerio.load(body);
+                }
+            };
+    
+    const get_zips = {
+        uri: 'https://www2.census.gov/geo/relfiles/cdsld18/natl/natl_zccd_delim.txt',
+        transform: function(body) {
+            return cheerio.load(body);
+            }
+        };
+    
+    try {
+        if (!(fs.existsSync('./reps.html'))) {
+            // if files do not exist
+            // *need to add a way to check for updates*
+            const $get_reps = await request(get_reps);
+            const $get_zips = await request(get_zips);
+
+            fs.writeFile("./reps.html", $get_reps.html(), function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+                console.log("The file reps.html was saved!");
+            })
+
+            fs.writeFile("./zips.html", $get_zips.html(), function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+                console.log("The file zips.html was saved!");
+            })
+        }
+        
+        const $reps = await cheerio.load(fs.readFileSync('./reps.html'), {
+            xmlMode: true
+        });
+        const $zips = await cheerio.load(fs.readFileSync('./zips.html'));
+
+        results = [$reps, $zips];
+        return results;
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
 // Step 2: Update geo question_id 23 answer with list of all possible reps
 
     // IF territory = territory has no (voting) Representatives in Congress OR name Delegate or Commissioner
     // IF Alaska || Delaware || D.C. || Montana || North Dakota || South Dakota || Vermont || Wyoming AT LARGE 1 Rep
 
-<member>
-    <statedistrict>ID01</statedistrict>
-    <member-info>
-        <official-name>
-            Russ Fulcher
-        </official-name>
-        <state postal-code="ID">
-            <state-fullname>Idaho</state-fullname>
-        </state>
-        <district>1st</district>
-    </member-info>
-</member>
+build_reps().then(results => {
+    
+    // results[0] = house members, results[1] = census zips
+    
+    // Part 1: build rep / district object from house.gov xml
 
-[location, question_id, [reps]]
+    // house member list
+    // [question_id, location, district_id, rep]
+    
+    // <member>
+    // <statedistrict>ID01</statedistrict>
+    // <member-info>
+    //     <official-name>
+    //         Russ Fulcher
+    //     </official-name>
+    //     <state postal-code="ID">
+    //         <state-fullname>Idaho</state-fullname>
+    //     </state>
+    //     <district>1st</district>
+    // </member-info>
+    // </member>
 
-// state_fips object
+    
+    const $replist = cheerio.load(results[0].html());
+    // console.log($replist.html());
+    // console.log(results[0]);
+    // console.log($replist.html());
 
-state_fips = {  06: "California",
-                01: "Alabama"
-             };
-
-// Step 2.5 - build rep / district object from house.gov xml
-
-reps = {"California": {
-            05: "Mike Thompson",
-            11: "MArk DeSaulnier"
-        },
-        "Alabama": {
-            06: "Gary J. Palmer",
-            07: "Terri A. Sewell"
-        },
-    };
-
-// Step 3: Build zip table 
-// location and zip as primary keys, ignore case when state is different
-// 06 = California
-list = [ [06,94803,05],
-         [06,94803,11] ];
-
-
- ['California', 06, 94803, [05, 11], ["Mike Thompson", "Mark DeSaulnier"]];
-
- [06, 94803, [05, 11]];
- 
- state_fips[06] = "California"
-
-object = {"California": {
-                94803: [05, 11]
-            },
-            "Alabama": {
-                35004: 03,
-                35005: [06, 07]
-            }
-        };
-        
-// 
-
-
-
-// zip match different state, ignore case?
-
-01,30165,03
-13,30165,14
+    $replist('member-info').each((i, el) => {
+        console.log($replist(el).find('official-name').text().length);
+        console.log($replist(el).find('official-name').text());
+        // if (($replist(el).find('official-name').length) === 0) {
+        //     console.log("the value is 0");
+        //     console.log($replist(el).find('statedistrict').text());
+            
+        // } else {
+        //     // console.log($replist(el).find('official-name').text());
+        // }
+        // console.log($replist(el).find('official-name').length();
+    });
+    
+    
 
 
 
 
-// with locations, rep names, and district id
-
-// Part 2: map all zip codes to congressional district and state
+// Part 2: Build zip table
 // compile states with all zip codes and congressional districts
 
+// location(state) and zip as primary keys, ignore case when state is different
+// 06 = California
 
-// zip codes unique, states or districts might not be
+// same zip different state
+// 01,30165,03
+// 13,30165,14
+
+//  ['California', 06, 94803, [05, 11], ["Mike Thompson", "Mark DeSaulnier"]];
+
+//  [06, 94803, [05, 11]];
+
+
+    
+//     create array of results from text, split by spacing
+//     remove title and description from array
+    
+//     let ziplist = results[1].html().split('\n');
+//     ziplist.shift(), ziplist.shift();
+
+//     for (rows in ziplist) {
+//         ziplist[rows] = ziplist[rows].split(',');
+//         // ziplist[rows][0] = fips[ziplist[rows][0]];
+//         // match code to fps object and insert state into row
+//         ziplist[rows].unshift(fips[ziplist[rows][0]]);
+//     };
+//     console.log(ziplist);
+
+})
+.catch(err => console.log(err));
 
 
 
-// build with 
+
+
+
+// Possible JSON objects
+
+// reps = {"California": {
+//             05: "Mike Thompson",
+//             11: "MArk DeSaulnier"
+//         },
+//         "Alabama": {
+//             06: "Gary J. Palmer",
+//             07: "Terri A. Sewell"
+//         },
+//     };
+
+
+// object = {"California": {
+//                 94803: [05, 11]
+//             },
+//             "Alabama": {
+//                 35004: 03,
+//                 35005: [06, 07]
+//             }
+//         };
+        
 
 
